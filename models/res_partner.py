b0d1037d541620a61eef75cdbc34034b4a45b010
jeffrepo <jeffer.leo93@gmail.com>
2021-10-19 15:19:08 -0600

    Cambiar tarifa por en base al estado

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
 import logging
 
 class ResPartner(models.Model):
@@ -11,3 +10,10 @@ class ResPartner(models.Model):
         ('afiliado_activo', 'Afiliado / Activo'),
         ('afiliado_moroso', 'Afiliado / Moroso'),
         ('afiliado_retirar', 'Afiliado / Por retirar')],'Estado', default='no_afiliado')
+
+    @api.onchange('estado')
+    def _onchange_estado(self):
+        if self.estado:
+            estado_ids = self.env['camaracomercio.config.estado'].search([('estado','=',self.estado)])
+            if estado_ids:
+                self.property_product_pricelist = estado_ids[0].tarifa_id.id
